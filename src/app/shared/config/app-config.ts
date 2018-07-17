379984e50da07be247df19607d138960ed45f460
nevil886 <nevil886@gmail.com>
2018-07-17 14:21:52 +0530

    Dynamic Forms

--- src/app/shared/config/app-config.ts
+++ src/app/shared/config/app-config.ts
@@ -5,7 +5,7 @@ export class AppConfig {
     ACCESS_PATH : ""
   };
 
-  public static WEB_API_URL = 'http://192.168.1.200:8070';
+  public static WEB_API_URL = 'http://192.168.1.200:6170';
   //public static WEB_API_URL = ''; //prod
   public static IMAGE_URL = "http://192.168.1.200:90/images/"; //dev
   //public static IMAGE_URL = "https://s3-ap-southeast-1.amazonaws.com/ammicoin.com/";  // pro
